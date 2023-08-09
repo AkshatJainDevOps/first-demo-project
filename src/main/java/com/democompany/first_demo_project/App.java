92e0f7927f1b7f7d41e1be606def8d27aa79508b
AkshatJainDevOps <140220195+AkshatJainDevOps@users.noreply.github.com>
2023-08-09 13:03:40 +0530

    Update App.java

--- src/main/java/com/democompany/first_demo_project/App.java
+++ src/main/java/com/democompany/first_demo_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Akshat Jain, Learning Jenkins." );
+        System.out.println( "Hello World! This is Akshat Jain, Learning Jenkins and it is amazing." );
     }
 }
